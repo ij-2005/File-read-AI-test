d743ed736e4f068678d1dbbdafe6d672f5a26b77
ij-2005 <officialij2005@gmail.com>
2026-04-01 22:47:53 +0800

    removed test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-const pdfParse = require("pdf-parse");
-
-(async () => {
-  const dataBuffer = fs.readFileSync("demo.pdf");
-  const data = await pdfParse(dataBuffer);
-  console.log(data.text);
-})();
